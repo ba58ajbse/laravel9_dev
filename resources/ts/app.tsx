d176d0d5da96d24a74b0f231c1387af919b404ba
shibuya-wsl2 <shibuya@pyxos-jk.co.jp>
2022-06-16 17:15:25 +0900

    react, typescript向けにファイル修正

---
+++ resources/ts/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+
+const App = () => {
+  return <div>Hello, World!</div>;
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
